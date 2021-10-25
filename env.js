a2eb6bc22fec49a7e5911ee423aabfb99be9aa07
LostButStillImproving <mike6353@edu.easj.dk>
2021-10-25 13:56:12 +0200

    change redirect url

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 export const CLIENT_ID = "f52afe402a954ce4a5b04efa7c315552"
-export const REDIRECT_URI = "http://localhost:3000"
+export const REDIRECT_URI = "https://spotify-stats-5typuqp64-lostbutstillimproving.vercel.app/"
 
 
 
